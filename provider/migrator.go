5b417d238ef51e2f692919edec98294f3d5e6d90
roneli <38083777+roneli@users.noreply.github.com>
2021-08-12 11:15:37 +0300

    delete tables before creation
    delete and cascade tables before creating them again.

--- provider/migrator.go
+++ provider/migrator.go
@@ -3,21 +3,15 @@ package provider
 import (
 	"context"
 	"fmt"
+	"github.com/jackc/pgx/v4"
 	"strconv"
 	"strings"
 
 	"github.com/huandu/go-sqlbuilder"
 
 	"github.com/cloudquery/cq-provider-sdk/provider/schema"
-	"github.com/georgysavva/scany/pgxscan"
 	"github.com/hashicorp/go-hclog"
 	"github.com/jackc/pgx/v4/pgxpool"
-	"github.com/thoas/go-funk"
-)
-
-const (
-	queryTableColumns = `SELECT array_agg(column_name::text) as columns FROM information_schema.columns WHERE table_name = $1`
-	addColumnToTable  = `ALTER TABLE %s ADD COLUMN IF NOT EXISTS %v %v;`
 )
 
 // Migrator handles creation of schema.Table in database if they don't exist, and migration of tables if provider was upgraded.
@@ -40,23 +34,22 @@ func (m Migrator) CreateTable(ctx context.Context, conn *pgxpool.Conn, t *schema
 		} else {
 			ctb.Define(c.Name, schema.GetPgTypeFromType(c.Type))
 		}
-
 	}
-
 	m.buildColumns(ctb, t.Columns, parent)
 	ctb.Define(fmt.Sprintf("constraint %s_pk primary key(%s)", schema.TruncateTableConstraint(t.Name), strings.Join(t.PrimaryKeys(), ",")))
 	sql, _ := ctb.BuildWithFlavor(sqlbuilder.PostgreSQL)
-
-	m.log.Debug("creating table if not exists", "table", t.Name)
-	if _, err := conn.Exec(ctx, sql); err != nil {
+	if err := conn.BeginFunc(ctx, func(tx pgx.Tx) error {
+		if _, err := conn.Exec(ctx, fmt.Sprintf("DROP TABLE IF EXISTS %s CASCADE", strconv.Quote(t.Name))); err != nil {
+			return err
+		}
+		m.log.Debug("creating table if not exists", "table", t.Name)
+		if _, err := conn.Exec(ctx, sql); err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
 		return err
 	}
-
-	m.log.Debug("migrating table columns if required", "table", t.Name)
-	if err := m.upgradeTable(ctx, conn, t); err != nil {
-		return err
-	}
-
 	if t.Relations == nil {
 		return nil
 	}
@@ -70,37 +63,6 @@ func (m Migrator) CreateTable(ctx context.Context, conn *pgxpool.Conn, t *schema
 		}
 	}
 	return nil
-}
-
-func (m Migrator) upgradeTable(ctx context.Context, conn *pgxpool.Conn, t *schema.Table) error {
-	rows, err := conn.Query(ctx, queryTableColumns, t.Name)
-	if err != nil {
-		return err
-	}
-
-	var existingColumns struct {
-		Columns []string
-	}
-
-	if err := pgxscan.ScanOne(&existingColumns, rows); err != nil {
-		return err
-	}
-
-	columnsToAdd, _ := funk.DifferenceString(t.ColumnNames(), existingColumns.Columns)
-	for _, d := range columnsToAdd {
-		m.log.Debug("adding column", "column", d)
-		col := t.Column(d)
-		if col == nil {
-			m.log.Warn("column missing from table, not adding it", "table", t.Name, "column", d)
-			continue
-		}
-		sql, _ := sqlbuilder.Buildf(addColumnToTable, sqlbuilder.Raw(t.Name), sqlbuilder.Raw(d), sqlbuilder.Raw(schema.GetPgTypeFromType(col.Type))).BuildWithFlavor(sqlbuilder.PostgreSQL)
-		if _, err := conn.Exec(ctx, sql); err != nil {
-			return err
-		}
-	}
-	return nil
-
 }
 
 func (m Migrator) buildColumns(ctb *sqlbuilder.CreateTableBuilder, cc []schema.Column, parent *schema.Table) {
