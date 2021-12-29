61bd89638377c75febaf6f219e070cb18713b544
Yevgeny Pats <yev.pats@gmail.com>
2021-12-29 11:54:32 +0200

    remove snapshot testing

--- provider/testing/resource.go
+++ provider/testing/resource.go
@@ -2,11 +2,8 @@ package testing
 
 import (
 	"context"
-	"encoding/json"
-	"errors"
 	"fmt"
 	"os"
-	"path"
 	"strings"
 	"sync"
 	"testing"
@@ -18,7 +15,6 @@ import (
 	"github.com/cloudquery/cq-provider-sdk/testlog"
 	"github.com/cloudquery/faker/v3"
 	"github.com/georgysavva/scany/pgxscan"
-	"github.com/google/go-cmp/cmp"
 	"github.com/hashicorp/go-hclog"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/stretchr/testify/assert"
@@ -79,22 +75,7 @@ func TestResource(t *testing.T, resource ResourceTestCase) {
 		t.Fatal(err)
 	}
 
-	if resource.SnapshotsDir != "" {
-
-		// run this if snapshot testing is enabled
-		equal, err := verifyTable(t, conn, resource.Table, resource)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if !equal {
-			t.Error("results not equal")
-		}
-	} else {
-		// run this if snapshot testing is disabled. i.e mock tests. This will probabbly go away
-		verifyNoEmptyColumns(t, resource, conn)
-	}
-
-	// verifyNoEmptyColumns(t, resource, conn)
+	verifyNoEmptyColumns(t, resource, conn)
 
 	if err := conn.Conn().Close(ctx); err != nil {
 		t.Fatal(err)
@@ -134,113 +115,6 @@ func fetch(t *testing.T, resource *ResourceTestCase) error {
 	}
 
 	return nil
-}
-
-func verifyTable(t *testing.T, conn *pgxpool.Conn, table *schema.Table, resource ResourceTestCase) (bool, error) {
-	t.Helper()
-	res := true
-	columnsToIgnore := []string{
-		"cq_id",
-		"meta",
-	}
-	// this order of insertion is not the same so we try to order by all columns which are constant
-	// Future note - if api will return results not in the same order we will have to do a smarter diff that doesn't rely on order
-	// this is not hard but just will provider worse debug info on what is changed
-	columns := ""
-	for _, c := range table.Columns {
-		if !ignoreColumns[c.Name] && !strings.HasSuffix(c.Name, "_cq_id") && !c.IgnoreInIntTests {
-			columns += "\"" + c.Name + "\"" + ","
-		} else {
-			columnsToIgnore = append(columnsToIgnore, c.Name)
-		}
-	}
-
-	columns = strings.TrimRight(columns, ",")
-	s := sq.StatementBuilder.
-		PlaceholderFormat(sq.Dollar).
-		Select(fmt.Sprintf("json_agg(%s order by %s)", table.Name, columns)).
-		From(table.Name)
-
-	query, args, err := s.ToSql()
-	if err != nil {
-		return false, err
-	}
-
-	var data []map[string]interface{}
-	if err := pgxscan.Get(context.Background(), conn, &data, query, args...); err != nil {
-		return false, err
-	}
-	removeColumns(data, columnsToIgnore)
-
-	b, err := json.MarshalIndent(data, "", "\t")
-	if err != nil {
-		return false, err
-	}
-
-	snapshotPath := path.Join(resource.SnapshotsDir, table.Name+".snapshot")
-	if err := os.MkdirAll(resource.SnapshotsDir, os.ModePerm); err != nil {
-		return false, err
-	}
-
-	// nolint
-	if _, err := os.Stat(snapshotPath); err == nil {
-		// snapshot already exist check if content is equal, if not fail
-		snapshotContent, err := os.ReadFile(snapshotPath)
-		if err != nil {
-			return false, err
-		}
-		var savedData []map[string]interface{}
-		if err := json.Unmarshal(snapshotContent, &savedData); err != nil {
-			return false, err
-		}
-
-		diff := cmp.Diff(data, savedData)
-		if diff != "" {
-			t.Log("found diff")
-			t.Log(diff)
-			t.Logf("Saving snapshot to %s.tmp\n", snapshotPath)
-			// open output file
-			if err := os.WriteFile(snapshotPath+".tmp", b, 0644); err != nil {
-				return false, err
-			}
-			res = false
-		}
-	} else if errors.Is(err, os.ErrNotExist) {
-		t.Logf("Previous snapshot doesn't exist. Saving snapshot to %s\n", snapshotPath)
-		// open output file
-		if err := os.WriteFile(snapshotPath, b, 0644); err != nil {
-			return false, err
-		}
-		res = false
-	} else {
-		return false, err
-	}
-
-	for _, relation := range table.Relations {
-		equal, err := verifyTable(t, conn, relation, resource)
-		if err != nil {
-			return false, err
-		}
-		if !equal {
-			res = false
-		}
-	}
-	return res, nil
-}
-
-func removeColumns(res []map[string]interface{}, ignoreColumns []string) {
-	ignoreColumnsMap := make(map[string]bool, len(ignoreColumns))
-	for _, c := range ignoreColumns {
-		ignoreColumnsMap[c] = true
-	}
-
-	for i := range res {
-		for c := range res[i] {
-			if ignoreColumnsMap[c] {
-				res[i][c] = "[unstable_column]"
-			}
-		}
-	}
 }
 
 func deleteTables(conn *pgxpool.Conn, table *schema.Table) error {
@@ -348,29 +222,3 @@ func getTablesFromMainTable(table *schema.Table) []string {
 	}
 	return res
 }
-
-// This should move to linter
-// func testTableIdentifiersForProvider(t *testing.T, prov *provider.Provider) {
-// 	t.Run("testTableIdentifiersForProvider", func(t *testing.T) {
-// 		t.Parallel()
-// 		for _, res := range prov.ResourceMap {
-// 			res := res
-// 			t.Run(res.Name, func(t *testing.T) {
-// 				testTableIdentifiers(t, res)
-// 			})
-// 		}
-// 	})
-// }
-
-// this should move to linter
-// func testTableIdentifiers(t *testing.T, table *schema.Table) {
-// 	t.Parallel()
-// 	assert.NoError(t, schema.ValidateTable(table))
-
-// 	for _, res := range table.Relations {
-// 		res := res
-// 		t.Run(res.Name, func(t *testing.T) {
-// 			testTableIdentifiers(t, res)
-// 		})
-// 	}
-// }
