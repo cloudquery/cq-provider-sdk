46a8b1adcad1863fe394625a8ed18eb7dfcd9240
Kemal Hadimli <disq@users.noreply.github.com>
2022-01-07 10:51:32 +0000

    Add cli handler

---
+++ provider/migrations/cli.go
@@ -0,0 +1,73 @@
+package migrations
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/cloudquery/cq-provider-sdk/provider"
+	"github.com/hashicorp/go-hclog"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+const defaultPath = "./resources/provider/migrations"
+
+// Run is the main entry point for CLI usage.
+func Run(p *provider.Provider, outputPath string) error {
+	const defaultPrefix = "unreleased_"
+
+	if outputPath == "" {
+		outputPath = defaultPath
+	}
+
+	outputPathParam := flag.String("path", outputPath, "Path to migrations directory")
+	prefixParam := flag.String("prefix", defaultPrefix, "Prefix for files")
+	doFullParam := flag.Bool("full", false, "Generate initial migrations (prefix will be 'init_')")
+	dsnParam := flag.String("dsn", os.Getenv("CQ_DSN"), "DSN to compare changes against")
+	flag.Parse()
+	if flag.NArg() > 0 {
+		flag.Usage()
+		return fmt.Errorf("more args than necessary")
+	}
+
+	if *doFullParam {
+		if *prefixParam == defaultPrefix {
+			*prefixParam = "init_"
+		}
+
+		if err := GenerateFull(context.Background(), hclog.L(), p, *outputPathParam, *prefixParam); err != nil {
+			return fmt.Errorf("failed to generate migrations: %w", err)
+		}
+		return nil
+	}
+
+	if *dsnParam == "" {
+		return fmt.Errorf("DSN not specified: Use -dsn or set CQ_DSN")
+	}
+
+	pool, err := connect(*dsnParam)
+	if err != nil {
+		return err
+	}
+	conn, err := pool.Acquire(context.Background())
+	if err != nil {
+		return err
+	}
+	defer conn.Release()
+
+	if err := GenerateDiff(context.Background(), hclog.L(), conn, p, *outputPathParam, *prefixParam); err != nil {
+		return fmt.Errorf("failed to generate migrations: %w", err)
+	}
+
+	return nil
+}
+
+func connect(dsn string) (*pgxpool.Pool, error) {
+	poolCfg, err := pgxpool.ParseConfig(dsn)
+	if err != nil {
+		return nil, err
+	}
+	poolCfg.LazyConnect = true
+	return pgxpool.ConnectConfig(context.Background(), poolCfg)
+}
