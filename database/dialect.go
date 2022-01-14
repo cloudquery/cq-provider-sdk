587ed0610c0f5a0594ad102f7fc624070a495af0
Kemal Hadimli <disq@users.noreply.github.com>
2022-01-14 18:36:41 +0000

    lint

--- database/dialect.go
+++ database/dialect.go
@@ -16,7 +16,7 @@ func DSNtoDialect(dsn string) (d schema.DialectType, newDSN string, err error) {
 	switch u.Scheme {
 	case "timescaledb", "tsdb", "timescale":
 		// TODO remove
-		fixedDSN := strings.Replace(u.String(), u.Scheme+"://", "postgres://", -1)
+		fixedDSN := strings.Replace(u.String(), u.Scheme+"://", "postgres://", 1)
 		return schema.TSDB, fixedDSN, nil
 	default:
 		return schema.Postgres, dsn, nil
