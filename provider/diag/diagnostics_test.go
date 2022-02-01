7fd490291cdc0a3f65e05ac00f6572850b366525
roneli <38083777+roneli@users.noreply.github.com>
2022-02-01 20:31:28 +0200

    more tests

--- provider/diag/diagnostics_test.go
+++ provider/diag/diagnostics_test.go
@@ -34,6 +34,60 @@ func TestDiagnostics_Squash(t *testing.T) {
 				},
 			},
 		},
+		{
+			Name: "simple squash w/details",
+			Value: Diagnostics{
+				NewBaseError(errors.New("error test"), ERROR, RESOLVING, "a", "some summary", "some details"),
+				NewBaseError(errors.New("error test"), ERROR, RESOLVING, "a", "some summary", "some details"),
+			},
+			Want: []FlatDiag{
+				{
+					Err:      "error test",
+					Resource: "a",
+					Type:     RESOLVING,
+					Severity: ERROR,
+					Summary:  "some summary",
+					Description: Description{
+						Resource: "a",
+						Summary:  "some summary",
+						Detail:   "Repeated[2]: some details",
+					},
+				},
+			},
+		},
+		{
+			Name: "different resource no squash",
+			Value: Diagnostics{
+				NewBaseError(errors.New("error test"), ERROR, RESOLVING, "a", "some summary", ""),
+				NewBaseError(errors.New("error test"), ERROR, RESOLVING, "b", "some summary", ""),
+			},
+			Want: []FlatDiag{
+				{
+					Err:      "error test",
+					Resource: "a",
+					Type:     RESOLVING,
+					Severity: ERROR,
+					Summary:  "some summary",
+					Description: Description{
+						Resource: "a",
+						Summary:  "some summary",
+						Detail:   "",
+					},
+				},
+				{
+					Err:      "error test",
+					Resource: "b",
+					Type:     RESOLVING,
+					Severity: ERROR,
+					Summary:  "some summary",
+					Description: Description{
+						Resource: "b",
+						Summary:  "some summary",
+						Detail:   "",
+					},
+				},
+			},
+		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.Name, func(t *testing.T) {
