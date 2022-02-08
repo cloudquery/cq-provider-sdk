bb534d15a41c42e1a9282afbe2f8dcd9fed0d14b
Yevgeny Pats <yev.pats@gmail.com>
2022-02-08 13:04:13 +0200

    Update helpers/limits.go
    Co-authored-by: Kemal <223029+disq@users.noreply.github.com>

--- helpers/limits.go
+++ helpers/limits.go
@@ -10,7 +10,7 @@ const GO_ROUTINES_PER_GB uint64 = 250000
 func GetMaxGoRoutines() uint64 {
 	totalMemory := memory.TotalMemory()
 	if totalMemory == 0 {
-		// than we assume we have 2 GB RAM
+		// assume we have 2 GB RAM
 		return GO_ROUTINES_PER_GB * 2
 	}
 	gb := float64(totalMemory) / float64(GB_IN_BYTES)
