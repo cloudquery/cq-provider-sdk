01d4515f4f570ce9dfae5c6d625edb684c7b3872
bbernays <ben@bernays.me>
2022-02-24 14:02:04 -0500

    add tests

--- helpers/limits.go
+++ helpers/limits.go
@@ -8,7 +8,14 @@ const GB_IN_BYTES uint64 = 1024 * 1024 * 1024
 const GO_ROUTINES_PER_GB uint64 = 250000
 
 func GetMaxGoRoutines() uint64 {
-	totalMemory := memory.TotalMemory()
+	return calculateGoRoutines(getMemory())
+}
+
+func getMemory() uint64 {
+	return memory.TotalMemory()
+}
+
+func calculateGoRoutines(totalMemory uint64) uint64 {
 	if totalMemory == 0 {
 		// assume we have 2 GB RAM
 		return GO_ROUTINES_PER_GB * 2
