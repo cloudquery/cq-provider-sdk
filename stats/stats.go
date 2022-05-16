db2ace525d35bf63144ffccf3762dfff3f780bad
erezrokah <erezrokah@users.noreply.github.com>
2022-05-16 18:27:13 +0300

    refactor: Use mutex instead of channel

--- stats/stats.go
+++ stats/stats.go
@@ -11,17 +11,6 @@ import (
 	"github.com/segmentio/stats/v4"
 )
 
-const (
-	bufferSize = 100
-)
-
-type measure struct {
-	id       string
-	duration time.Duration
-	stamp    bool
-	time     time.Time
-}
-
 type stat struct {
 	Start    time.Time
 	Duration time.Duration
@@ -29,7 +18,6 @@ type stat struct {
 
 type logHandler struct {
 	logger            hclog.Logger
-	measures          chan measure
 	trackedOperations *orderedmap.OrderedMap
 	mu                sync.Mutex
 }
@@ -69,9 +57,18 @@ func meta(name string, tags []stats.Tag) (string, bool) {
 // Or by `clock.Stop` which indicates a "stop" of an operation
 // We pass the measurements to a channel and periodically aggregate the data and print a hearbeat log
 func (h *logHandler) HandleMeasures(time time.Time, measures ...stats.Measure) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
 	for _, m := range measures {
 		id, stamp := meta(m.Fields[0].Name, m.Tags)
-		h.measures <- measure{id: id, duration: m.Fields[0].Value.Duration(), time: time, stamp: stamp}
+		if stamp {
+			item, ok := h.trackedOperations.Get(id)
+			if ok {
+				h.trackedOperations.Set(id, stat{Start: item.(stat).Start, Duration: m.Fields[0].Value.Duration()})
+			}
+		} else {
+			h.trackedOperations.Set(id, stat{Start: time, Duration: 0})
+		}
 	}
 }
 
@@ -79,24 +76,6 @@ func (h *logHandler) HandleMeasures(time time.Time, measures ...stats.Measure) {
 func (h *logHandler) Flush() {
 	h.mu.Lock()
 	defer h.mu.Unlock()
-
-	hasItems := true
-	for hasItems {
-		select {
-		case m := <-h.measures:
-			// We group start and stop measurements. Stop means `clock.Stop` was called for the operation
-			if m.stamp {
-				item, ok := h.trackedOperations.Get(m.id)
-				if ok {
-					h.trackedOperations.Set(m.id, stat{Start: item.(stat).Start, Duration: m.duration})
-				}
-			} else {
-				h.trackedOperations.Set(m.id, stat{Start: m.time, Duration: 0})
-			}
-		default:
-			hasItems = false
-		}
-	}
 
 	durationReported := make([]string, 0)
 	for el := h.trackedOperations.Front(); el != nil; el = el.Next() {
@@ -158,5 +137,5 @@ func Flush() {
 }
 
 func newHandler(logger hclog.Logger) stats.Handler {
-	return &logHandler{logger: logger, measures: make(chan measure, bufferSize), trackedOperations: orderedmap.NewOrderedMap()}
+	return &logHandler{logger: logger, trackedOperations: orderedmap.NewOrderedMap()}
 }
