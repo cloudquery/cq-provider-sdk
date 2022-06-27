962dca5db4ad639d9abd94580593b4c1ca1c32d0
Kemal Hadimli <disq@users.noreply.github.com>
2022-06-27 11:54:13 +0100

    Cleaner

--- provider/provider.go
+++ provider/provider.go
@@ -107,30 +107,18 @@ func (p *Provider) GetProviderConfig(_ context.Context, req *cqproto.GetProvider
 
 		data := &yaml.Node{
 			Kind: yaml.MappingNode,
-			// HeadComment doesn't work here
 			Content: []*yaml.Node{
 				{
 					Kind:  yaml.ScalarNode,
 					Value: "configuration",
 				},
 				{
-					Kind:        yaml.MappingNode,
-					HeadComment: strings.TrimRight(providerConfig.Example(), "\r\n") + "\n",
-					Content: []*yaml.Node{
-						{
-							Kind:        yaml.ScalarNode,
-							Value:       "example-key",
-							LineComment: "This is an example, can be removed",
-						},
-						{
-							Kind:  yaml.ScalarNode,
-							Value: "example-value",
-						},
-					},
+					Kind: yaml.ScalarNode,
+					Tag:  "!!null",
 				},
 				{
 					Kind:        yaml.ScalarNode,
-					HeadComment: "list of resources to fetch",
+					HeadComment: strings.TrimRight(providerConfig.Example(), "\r\n") + "\n \nlist of resources to fetch",
 					Value:       "resources",
 				},
 				{
